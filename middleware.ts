e874b20d89935b859209d060365086b93599728f
arkoroy05 <132930851+arkoroy05@users.noreply.github.com>
2024-12-17 10:20:25 +0530

    implemented signin, signup pages

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
-    "/signin",
-    "/signout",
+    "/sign-in",
+    "/sign-out",
     "/",
     "/home"
 ])
@@ -25,10 +25,10 @@ export default clerkMiddleware((auth,req)=>{
     //not logged in
     if(!userId){
         if(!isPublicRoute(req) && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/signin",req.url)) 
+            return NextResponse.redirect(new URL("/sign-in",req.url)) 
         }
         if(isApiRequest && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/signin",req.url))
+            return NextResponse.redirect(new URL("/sign-in",req.url))
         }
 }
 return NextResponse.next()
